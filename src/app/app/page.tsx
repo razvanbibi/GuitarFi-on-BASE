89cc950deb143e49691058840fd042c8a9a03159
razvanbibi <mstrazvanbibi@gmail.com>
2026-07-01 17:30:09 +0600

    Deploying GuitarFi project docs and typescripts fot BASE version

--- src/app/app/page.tsx
+++ src/app/app/page.tsx
@@ -36,6 +36,7 @@ type Supporter = {
   avatar?: string;
 };
 
+
 const USDM_TOKEN_ADDRESS = "0x765DE816845861e75A25fCA122bb6898B8B1282a";
 
 function AvatarBubbleStream({ avatar }: { avatar: string }) {
